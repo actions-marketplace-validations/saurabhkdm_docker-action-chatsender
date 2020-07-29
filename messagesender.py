d78073252dcb69ac1c8c58ad18bd2b31e79973f2
Saurabh Kadam <66935356+saurabhkdm@users.noreply.github.com>
2020-07-29 15:22:09 +0530

    Update messagesender.py

--- messagesender.py
+++ messagesender.py
@@ -5,7 +5,7 @@ from httplib2 import Http
 
 def main():
     """Hangouts Chat incoming webhook quickstart."""
-    msg = sys.argv[1]
+    msg = sys.argv[1:]
     url = 'https://chat.googleapis.com/v1/spaces/AAAAW92x_Vw/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=YhnS-MnqheNDK2l4rdddHVN2SZNN_0UbhkLQCZIQ2bM%3D'
     bot_message = {
         'text' : msg}

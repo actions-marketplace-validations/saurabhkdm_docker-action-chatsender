11a1de188a34099cc9c2eb9650de9c7ece11963e
Saurabh Kadam <66935356+saurabhkdm@users.noreply.github.com>
2020-08-21 18:18:39 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -l
 
-python /app/messagesender.py ${{github.event.number}}
+pull_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+
+
+
+python /app/messagesender.py $pull_number
